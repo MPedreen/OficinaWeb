689d5c46b5f88d8c4c623b054250904cf5cc82e9
Pedro Ferreira <pedrophpvp12@gmail.com>
2021-09-13 16:50:09 -0300

    Configurando String de Conexao e instalando o entityframeworkcore

--- OficinaWeb/Models/Carro.cs
+++ OficinaWeb/Models/Carro.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace OficinaWeb.Models
 {
     public class Carro
     {
+        [Key]
         public int Id { get; set; }
         public string ModeloCarro { get; set; }
         public string Marca { get; set; }
