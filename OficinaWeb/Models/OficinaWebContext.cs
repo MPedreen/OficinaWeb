cd534dcdea64f55d0cb1ba4ec56675e2f7ff120a
Pedro Ferreira <pedrophpvp12@gmail.com>
2021-09-13 12:04:23 -0300

    Habilitando o Razor Runtime Compilation

---
+++ OficinaWeb/Models/OficinaWebContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace OficinaWeb.Models
+{
+    public class OficinaWebContext
+    {
+       
+    }
+}
--- OficinaWeb/Startup.cs
+++ OficinaWeb/Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using OficinaWeb.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,7 +24,8 @@ namespace OficinaWeb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
