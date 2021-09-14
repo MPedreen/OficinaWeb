72ffb4273e2dfc3b138faccfd899ada6965c8c52
Pedro Ferreira <pedrophpvp12@gmail.com>
2021-09-14 19:48:33 -0300

    Finalizando o botao e metodo Deletar

--- OficinaWeb/Controllers/CarrosController.cs
+++ OficinaWeb/Controllers/CarrosController.cs
@@ -37,7 +37,37 @@ namespace OficinaWeb.Controllers
                 return RedirectToAction("List");
             }
             return View(obj);
-         
         }
+
+        //GET - Delete
+        public IActionResult Delete(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Carros.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+
+        }
+        //POST - Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Carros.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            _db.Carros.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("List");
+        }
+
     }
 }
