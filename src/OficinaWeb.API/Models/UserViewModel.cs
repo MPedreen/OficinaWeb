a151948235f0dcf33910b8e325d82d8614a23ba5
Pedro Ferreira <pedrophpvp12@gmail.com>
2021-12-08 22:25:50 -0300

    criando view login

--- src/OficinaWeb.API/Models/UserViewModel.cs
+++ src/OficinaWeb.API/Models/UserViewModel.cs
@@ -2,6 +2,7 @@ namespace OficinaWeb.Models
 {
     public class UserViewModel
     {
+        public string NomeUsuario { get; set; }
         public string Email { get; set; }
         public string Senha { get; set; }
     }
